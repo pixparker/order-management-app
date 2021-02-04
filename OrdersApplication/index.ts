452378d618fba13dba03526a662d7ab7f2c45ec0
Ali Bazdaran <Ali.b@xecorp.co>
2021-02-04 15:34:17 +0000

    initial express and tsc

---
+++ OrdersApplication/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+
+const app = express();
+const PORT = 8000;
+app.get('/', (req,res) => res.send( ' Hi Express + TypeScript Server'));
+app.listen(PORT, () => {
+  console.log(`⚡️[server]:--- Server is running at https://localhost:${PORT}`);
+});
