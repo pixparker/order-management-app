626434e06e30772cead17233145744c42d83eb8e
Ali Bazdaran <Ali.b@xecorp.co>
2021-02-07 13:24:35 +0000

    adminPage: mock login

---
+++ AdminPanel/src/app/services/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService  implements CanActivate  {
+  
+
+  public hasUser: boolean = false;
+  
+  constructor(
+    public router: Router
+    ) { }
+
+    
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    if (!this.isAuthenticated()) {
+      this.router.navigate(['login']);
+      this.hasUser = false;
+      return false;
+    }
+    this.hasUser = true;
+    return true;
+  }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('token');
+    //mock auth
+    return token? true: false;
+  }
+
+
+  public async login(){
+    localStorage.setItem('token','mock-token');
+    this.hasUser = true;
+  }
+
+  public async logout(){
+    localStorage.removeItem('token');
+    this.hasUser = false;
+  }
+
+}
